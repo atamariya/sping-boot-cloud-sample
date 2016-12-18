05be24ef0bd1f4610f9c9ab382d0982a7dba2868
Anand Tamariya <antamari@in.ibm.com>
2016-12-18 20:04:10 +0530

    Added @PathVariable

--- src/main/java/org/spring/start/AppController.java
+++ src/main/java/org/spring/start/AppController.java
@@ -1,6 +1,7 @@
 package org.spring.start;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +15,15 @@ public class AppController {
 	ExternalRESTService service;
 
 	@RequestMapping("/")
-	public String home1() {
+	public String home() {
+		return config.name;
+	}
+
+	@RequestMapping("/search/{param}")
+	public String home1(@PathVariable String param) {
 //		Properties p = config.cloud().getCloudProperties();
 //		System.out.println(p);
-		String name = service.search("india");
+		String name = service.search(param);
 		return name;
 	}
 }
