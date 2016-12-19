37c06c32d2db429ec520d80b5eadac6ce0d4d97c
Anand Tamariya <antamari@in.ibm.com>
2016-12-19 19:08:28 +0530

    cleanup

--- src/main/java/org/spring/Application.java
+++ src/main/java/org/spring/Application.java
@@ -2,10 +2,9 @@ package org.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.java.AbstractCloudConfig;
 
 @SpringBootApplication
-public class Application extends AbstractCloudConfig {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
